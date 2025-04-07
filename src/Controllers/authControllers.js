5a6ca27bc4e48c6f1f9a7d6e6b7cd330bac7de4e
Evan-Codes88 <Evan2401tm@gmail.com>
2025-04-07 13:08:58 +1000

    Feat: Started Signup Controller

--- src/Controllers/authControllers.js
+++ src/Controllers/authControllers.js
@@ -1,6 +1,20 @@
+import { User } from '../Models/UserModel.js';
+import { sendErrorResponse, sendSuccessResponse } from '../Utils/utils.js';
 
 export const signup = async (request, response) => {
-    response.send("Signup Route");
+    const { fullName, email, password } = request.body;
+    try {
+        if (!fullName || !email || !password) {
+            return sendErrorResponse(response, 400, "All fields are required");
+        };
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return sendErrorResponse(response, 400, "Email is already in use");
+        }
+    } catch (error) {
+        
+    }
 };
 
 export const login = async (request, response) => {
